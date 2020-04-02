08bd1cfb3163ef76a876f60d094bad1a7d6beca3
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2020-04-01 21:06:23 -0400

    Initial Commit

---
+++ Assets/_Scripts/MegaManController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+
+public class MegaManController : MonoBehaviour
+{
+    public Animator controller;
+    public SpriteRenderer renderer;
+    public Transform spawnPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = spawnPoint.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // move to the right
+        if (Input.GetAxis("Horizontal") > 0.1f)
+        {
+            // run animation
+            controller.SetInteger("AnimState", 1);
+            renderer.flipX = false;
+        }
+
+        if (Input.GetAxis("Horizontal") == 0.0f)
+        {
+            // idle animation
+            controller.SetInteger("AnimState", 0);
+        }
+
+        if (Input.GetAxis("Horizontal") < -0.1f)
+        {
+            // run animation
+            controller.SetInteger("AnimState", 1);
+            renderer.flipX = true;
+        }
+
+        if (Input.GetAxis("Jump") > 0.1f)
+        {
+            //controller.SetInteger("AnimState", 2);
+        }
+    }
+}
